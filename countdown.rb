c554e750b3dca36a07092ac43c61036ee4a5f5d0
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-24 01:57:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,14 @@ def countdown(x_sec)
   end
   return "HAPPY NEW YEAR!"
 end
+
+x_sec = 10
+
+def countdown_with_sleep(x_sec)
+  while x_sec > 0
+    puts "#{x_sec} SECOND(S)!"
+    x_sec -= 1
+    sleep
+  end
+  return "HAPPY NEW YEAR!"
+end
