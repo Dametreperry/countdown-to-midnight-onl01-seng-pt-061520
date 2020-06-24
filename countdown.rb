bd6f61543154f2c709517a27fdc9b73a175c92ee
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-24 01:19:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,9 @@
 countdown = 12
-
-def countdown(x)
   while countdown > 0
-  countdown -= 1
   puts "#{countdown} SECONDS"
-end
+  countdown -= 1
 end
   
-  puts "HAPPY NEW YEAR
+  puts "HAPPY NEW YEAR"
   
-  x = 1
-while x < 10
-  puts "#{x} is less than 10"
-  x += 1
-end
+ 
